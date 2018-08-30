e0ea982aa363ce51524875c9c2dbf3a4389e26bb
Alex Bennett <benedictus.ab@gmail.com>
2018-08-30 09:37:12 -0600

    slider

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 6000
+});
